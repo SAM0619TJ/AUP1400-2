bb3ca539bce96d09777302ea5fbae6f689657b02
SAM0619TJ <genshin2006p@outlook.com>
2025-04-27 14:36:24 +0800

    指针，数组，函数

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
-
+#include"include/function.hpp"
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    std::cin.get();
+    int arr[8] = {1,5,4,8,7,6,3,2};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    bubbleSort(arr,len);
+    for(int i = 0; i < len ; i++)
+    {
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+
     return 0;
+
 }
